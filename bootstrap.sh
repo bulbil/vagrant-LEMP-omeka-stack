2fc42fb465615f7b8a2af55002f0ca999dd58898
Nabil Kashyap <n@nabilk.com>
2015-04-29 11:16:25 -0400

    bootstrap updated to create database, user, copy over required files

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,34 +19,36 @@ do
     if [ "${i}" = "mysql-server" ]
     then
         echo " ******* set selections ******** "
-#        debconf-set-selections <<< 'mysql-server mysql-server/root_password password omekaDev'
-#        debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password omekaDev'
+        debconf-set-selections <<< 'mysql-server mysql-server/root_password password omekaDev'
+        debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password omekaDev'
     fi    
-#    apt-get -y install "${i}"
+    apt-get -y install "${i}"
 done
 
 echo "sed **********************"
-# sed -i s/\;cgi\.fix_pathinfo\s*\=\s*1/cgi.fix_pathinfo\=0/ /etc/php5/fpm/php.ini
-# service php5-fpm restart
+ sed -i s/\;cgi\.fix_pathinfo\s*\=\s*1/cgi.fix_pathinfo\=0/ /etc/php5/fpm/php.ini
+ service php5-fpm restart
 
 echo "nginx configure **********************"
-# mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
-# cp /vagrant/default /etc/nginx/sites-available/default
+ mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+ cp /vagrant/default /etc/nginx/sites-available/default
 
 
 echo "start services **********************"
-# service nginx restart
+ service nginx restart
 
 echo "wget omeka"
-# wget https://github.com/omeka/Omeka/archive/master.zip -P /vagrant/webroot/
-# unzip /vagrant/webroot/master.zip -d /vagrant/webroot/
-# mv /vagrant/webroot/Omeka-master/* /vagrant/webroot/
-# rm -r /vagrant/webroot/Omeka-master
-# rm /vagrant/webroot/db.ini.changeme
-# cp /vagrant/db.ini /vagrant/webroot/
-# rm /vagrant/webroot/application/config/config.ini.changeme
-# cp /vagrant/config.ini /vagrant/webroot/application/config/
-# chmod -R 775 /vagrant/webroot/application/logs
+ wget https://github.com/omeka/Omeka/archive/master.zip -P /vagrant/webroot/
+ unzip /vagrant/webroot/master.zip -d /vagrant/webroot/
+ mv /vagrant/webroot/Omeka-master/* /vagrant/webroot/
+ rm -r /vagrant/webroot/Omeka-master
+ rm /vagrant/webroot/db.ini.changeme
+ cp /vagrant/db.ini /vagrant/webroot/
+ rm /vagrant/webroot/application/config/config.ini.changeme
+ cp /vagrant/config.ini /vagrant/webroot/application/config/
+ mv /vagrant/webroot/application/config/logs/errors.log.empty /vagrant/webroot/application/config/logs/errors.log
+ chmod -R 775 /vagrant/webroot/application/logs
+ cp /vagrant/.htaccess /vagrant/webroot/
 
 echo "mysql setup"
 mysql -u root -pomekaDev --execute="CREATE DATABASE IF NOT EXISTS omeka CHARACTER SET utf8 COLLATE utf8_unicode_ci";
